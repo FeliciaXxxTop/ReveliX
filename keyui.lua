5256d007cfee3666e09ad1384acce186ad2844ac
FeliciaXxxTop <feliciaxxxontop@gmail.com>
2025-01-20 13:26:42 +0700

    Update keyui.lua

--- keyui.lua
+++ keyui.lua
@@ -26,100 +26,100 @@ if isfile("Key.txt") and (Config:Verify_Key(readfile("Key.txt")) == trueData or 
   _auto()
   loadstring(game:HttpGet("https://raw.githubusercontent.com/NotB1itz/ReveliX/refs/heads/main/Ui.lua"))()
 else
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local UICorner = Instance.new("UICorner")
-local CheckKey = Instance.new("TextButton")
-local UICorner_2 = Instance.new("UICorner")
-local getKeyButton = Instance.new("TextButton")
-local UICorner_3 = Instance.new("UICorner")
-local TextLabel = Instance.new("TextLabel")
-local KeyTextBox = Instance.new("TextBox")
-local UICorner_4 = Instance.new("UICorner")
-local resultLabel = Instance.new("TextLabel")
-
-
-
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(43, 47, 53)
-Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.304147989, 0, 0.21354194, 0)
-Frame.Size = UDim2.new(0, 259, 0, 170)
-
-UICorner.Parent = Frame
-
-CheckKey.Name = "Check Key"
-CheckKey.Parent = Frame
-CheckKey.BackgroundColor3 = Color3.fromRGB(62, 67, 76)
-CheckKey.BorderColor3 = Color3.fromRGB(0, 0, 0)
-CheckKey.BorderSizePixel = 0
-CheckKey.Position = UDim2.new(0.0694980696, 0, 0.59632355, 0)
-CheckKey.Size = UDim2.new(0, 102, 0, 50)
-CheckKey.AutoButtonColor = false
-CheckKey.Font = Enum.Font.SourceSans
-CheckKey.Text = "Check key"
-CheckKey.TextColor3 = Color3.fromRGB(254, 254, 254)
-CheckKey.TextSize = 14.000
-
-UICorner_2.Parent = CheckKey
-
-getKeyButton.Name = "getKeyButton"
-getKeyButton.Parent = Frame
-getKeyButton.BackgroundColor3 = Color3.fromRGB(62, 67, 76)
-getKeyButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-getKeyButton.BorderSizePixel = 0
-getKeyButton.Position = UDim2.new(0.536679566, 0, 0.59632355, 0)
-getKeyButton.Size = UDim2.new(0, 102, 0, 50)
-getKeyButton.AutoButtonColor = false
-getKeyButton.Font = Enum.Font.SourceSans
-getKeyButton.Text = "Get key"
-getKeyButton.TextColor3 = Color3.fromRGB(254, 254, 254)
-getKeyButton.TextSize = 14.000
-
-UICorner_3.Parent = getKeyButton
-
-TextLabel.Parent = Frame
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.BorderSizePixel = 0
-TextLabel.Position = UDim2.new(0.111969113, 0, 0, 0)
-TextLabel.Size = UDim2.new(0, 200, 0, 24)
-TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "ReveliX Key System"
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.TextSize = 14.000
-
-KeyTextBox.Name = "KeyTextBox"
-KeyTextBox.Parent = Frame
-KeyTextBox.BackgroundColor3 = Color3.fromRGB(62, 67, 76)
-KeyTextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
-KeyTextBox.BorderSizePixel = 0
-KeyTextBox.Position = UDim2.new(0.0689999983, 0, 0.170000002, 0)
-KeyTextBox.Size = UDim2.new(0, 222, 0, 51)
-KeyTextBox.Font = Enum.Font.SourceSans
-KeyTextBox.Text = ""
-KeyTextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
-KeyTextBox.TextSize = 14.000
-
-UICorner_4.Parent = KeyTextBox
-
-resultLabel.Name = "resultLabel"
-resultLabel.Parent = Frame
-resultLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-resultLabel.BackgroundTransparency = 1.000
-resultLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-resultLabel.BorderSizePixel = 0
-resultLabel.Position = UDim2.new(0.108108111, 0, 0.464705884, 0)
-resultLabel.Size = UDim2.new(0, 200, 0, 24)
-resultLabel.Font = Enum.Font.SourceSans
-resultLabel.Text = "just do the key lazy"
-resultLabel.TextColor3 = Color3.fromRGB(173, 173, 173)
-resultLabel.TextSize = 12.000
+  local ScreenGui = Instance.new("ScreenGui")
+  local Frame = Instance.new("Frame")
+  local UICorner = Instance.new("UICorner")
+  local checkKeyButton = Instance.new("TextButton")
+  local UICorner_2 = Instance.new("UICorner")
+  local getKeyButton = Instance.new("TextButton")
+  local UICorner_3 = Instance.new("UICorner")
+  local TextLabel = Instance.new("TextLabel")
+  local KeyTextBox = Instance.new("TextBox")
+  local UICorner_4 = Instance.new("UICorner")
+  local resultLabel = Instance.new("TextLabel")
+  
+  --Properties:
+  
+  ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+  ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+  
+  Frame.Parent = ScreenGui
+  Frame.BackgroundColor3 = Color3.fromRGB(43, 47, 53)
+  Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+  Frame.BorderSizePixel = 0
+  Frame.Position = UDim2.new(0.304147989, 0, 0.21354194, 0)
+  Frame.Size = UDim2.new(0, 259, 0, 170)
+  
+  UICorner.Parent = Frame
+  
+  checkKeyButton.Name = "checkKeyButton"
+  checkKeyButton.Parent = Frame
+  checkKeyButton.BackgroundColor3 = Color3.fromRGB(62, 67, 76)
+  checkKeyButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+  checkKeyButton.BorderSizePixel = 0
+  checkKeyButton.Position = UDim2.new(0.0694980696, 0, 0.59632355, 0)
+  checkKeyButton.Size = UDim2.new(0, 102, 0, 50)
+  checkKeyButton.AutoButtonColor = false
+  checkKeyButton.Font = Enum.Font.SourceSans
+  checkKeyButton.Text = "Check key"
+  checkKeyButton.TextColor3 = Color3.fromRGB(254, 254, 254)
+  checkKeyButton.TextSize = 14.000
+  
+  UICorner_2.Parent = checkKeyButton
+  
+  getKeyButton.Name = "getKeyButton"
+  getKeyButton.Parent = Frame
+  getKeyButton.BackgroundColor3 = Color3.fromRGB(62, 67, 76)
+  getKeyButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+  getKeyButton.BorderSizePixel = 0
+  getKeyButton.Position = UDim2.new(0.536679566, 0, 0.59632355, 0)
+  getKeyButton.Size = UDim2.new(0, 102, 0, 50)
+  getKeyButton.AutoButtonColor = false
+  getKeyButton.Font = Enum.Font.SourceSans
+  getKeyButton.Text = "Get key"
+  getKeyButton.TextColor3 = Color3.fromRGB(254, 254, 254)
+  getKeyButton.TextSize = 14.000
+  
+  UICorner_3.Parent = getKeyButton
+  
+  TextLabel.Parent = Frame
+  TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+  TextLabel.BackgroundTransparency = 1.000
+  TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+  TextLabel.BorderSizePixel = 0
+  TextLabel.Position = UDim2.new(0.111969113, 0, 0, 0)
+  TextLabel.Size = UDim2.new(0, 200, 0, 24)
+  TextLabel.Font = Enum.Font.SourceSans
+  TextLabel.Text = "ReveliX Key System"
+  TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+  TextLabel.TextSize = 14.000
+  
+  KeyTextBox.Name = "KeyTextBox"
+  KeyTextBox.Parent = Frame
+  KeyTextBox.BackgroundColor3 = Color3.fromRGB(62, 67, 76)
+  KeyTextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+  KeyTextBox.BorderSizePixel = 0
+  KeyTextBox.Position = UDim2.new(0.0689999983, 0, 0.170000002, 0)
+  KeyTextBox.Size = UDim2.new(0, 222, 0, 51)
+  KeyTextBox.Font = Enum.Font.SourceSans
+  KeyTextBox.Text = ""
+  KeyTextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
+  KeyTextBox.TextSize = 14.000
+  
+  UICorner_4.Parent = KeyTextBox
+  
+  resultLabel.Name = "resultLabel"
+  resultLabel.Parent = Frame
+  resultLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+  resultLabel.BackgroundTransparency = 1.000
+  resultLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+  resultLabel.BorderSizePixel = 0
+  resultLabel.Position = UDim2.new(0.108108111, 0, 0.464705884, 0)
+  resultLabel.Size = UDim2.new(0, 200, 0, 24)
+  resultLabel.Font = Enum.Font.SourceSans
+  resultLabel.Text = "just do the key Devs are broke"
+  resultLabel.TextColor3 = Color3.fromRGB(173, 173, 173)
+  resultLabel.TextSize = 12.000
 
 getKeyButton.MouseButton1Click:Connect(function()
     local Succ = pcall(function()
@@ -136,7 +136,6 @@ getKeyButton.MouseButton1Click:Connect(function()
     
     if (Config:Verify_Key(enteredKey) == trueData or Config:Verify_PremiumKey(enteredKey) == trueData) then
       resultLabel.Text = "Key is valid!"
-      resultLabel.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
       keyTextBox.Text = ""
       screenGui:Destroy()
       pcall(writefile, "Key.txt", tostring(enteredKey))
@@ -145,7 +144,6 @@ getKeyButton.MouseButton1Click:Connect(function()
       _auto();
     else
       resultLabel.Text = "Invalid Key!"
-      keyTextBox.Text = "Invalid Key"
     end
   end)
 end
